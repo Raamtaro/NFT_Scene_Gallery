dd3aee28886bdd1d3ef0c0591b612d2e22f91b17
Raam <raam.sanghani@gmail.com>
2025-05-05 11:54:35 -0500

    Removed the sceneConfig.ts file because it was causing the build to fail. I will have to keep track of that to fix at a later date. The idea behind the sceneConfig.ts module was to free up some space in the Experience class under the setUpPairings() method. Right now I am hardcoding in the pairing configurations, but it might be nice to have these set up in a separate module so that I can just plug it in once into this Experience method and make the code less bulky.

--- src/experience/sceneConfig.ts
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three'
-import { GLTF } from "three/examples/jsm/Addons.js"
-
-import Experience from "./experience";
-import ParticleSystem from './particles/main/particleSystem';
-import DisplayCase from './glass/DisplayCase';
-
-
